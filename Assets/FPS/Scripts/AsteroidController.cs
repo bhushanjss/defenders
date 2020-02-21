a7b45a68957c0be9c46bb02c370f98676358abc6
Vidya <bhushanjss@gmail.com>
2020-02-21 00:39:24 +0000

    init asteroid

---
+++ Assets/FPS/Scripts/AsteroidController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+    [SerializeField]
+    private float rangeXZ= 100f;
+    [SerializeField]
+    private float rangeMinY = 100f;
+    [SerializeField]
+    private float rangeMaxY = 500f;
+    private float sizeRange = 10;
+    void Start()
+    {
+        float randomPosX = Random.Range(-rangeXZ, rangeXZ);
+        float randomPosZ = Random.Range(-rangeXZ, rangeXZ);
+        float randomPosY = Random.Range(rangeMinY, rangeMaxY);
+        float randomSize = Random.Range(1f, sizeRange);
+        transform.position = new Vector3(randomPosX, randomPosY, randomPosZ);
+        transform.localScale = new Vector3(randomSize, randomSize, randomSize);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        
+    }
+}
