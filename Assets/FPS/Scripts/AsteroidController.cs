34c95743060ecb7ac042e01948c4647ec9ca3cd9
Vidya <bhushanjss@gmail.com>
2020-02-22 14:57:08 +0000

    add multiple rocks

--- Assets/FPS/Scripts/AsteroidController.cs
+++ Assets/FPS/Scripts/AsteroidController.cs
@@ -5,21 +5,17 @@ using UnityEngine;
 public class AsteroidController : MonoBehaviour
 {
     [SerializeField]
+    private float yThreshold = -100f;
+    [SerializeField]
     private float _speed = 4.0f;
     [SerializeField]
-    private float rangeXZ= 100f;
-    [SerializeField]
-    private float rangeMinY = 100f;
-    [SerializeField]
-    private float rangeMaxY = 500f;
     private float sizeRange = 10;
+    private int _hit = 0;
+    private int _miss = 0;
+
     void Start()
     {
-        float randomPosX = Random.Range(-rangeXZ, rangeXZ);
-        float randomPosZ = Random.Range(-rangeXZ, rangeXZ);
-        float randomPosY = Random.Range(rangeMinY, rangeMaxY);
         float randomSize = Random.Range(1f, sizeRange);
-        transform.position = new Vector3(randomPosX, randomPosY, randomPosZ);
         transform.localScale = new Vector3(randomSize, randomSize, randomSize);
     }
 
@@ -27,17 +23,21 @@ public class AsteroidController : MonoBehaviour
     void Update()
     {
         //transform.Translate(Vector3.down * _speed * Time.deltaTime);
-        
+
+        if(transform.position.y < yThreshold)
+        {
+            Destroy(this.gameObject);
+            _miss++;
+        }        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Hit: "+ other.tag);
-
         if(other.tag == "Missile")
         {
             Destroy(other.gameObject);
             Destroy(this.gameObject);
+            _hit++;
         }
     }
 }
---
+++ Assets/FPS/Scripts/AsteroidsManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidsManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject asteroid1B;
+    [SerializeField]
+    private GameObject asteroid1C;
+    [SerializeField]
+    private GameObject asteroid1D;
+    [SerializeField]
+    private GameObject asteroid1E;
+    [SerializeField]
+    private GameObject asteroid4A;
+    [SerializeField]
+    private GameObject asteroid4B;
+    [SerializeField]
+    private GameObject asteroid5B;
+    [SerializeField]
+    private GameObject asteroid6A;
+    [SerializeField]
+    private GameObject asteroid6B;
+    [SerializeField]
+    private GameObject asteroid6C;
+    [SerializeField]
+    private GameObject asteroidContainer;
+    
+    [SerializeField]
+    private float rangeXZ = 100f;
+    [SerializeField]
+    private float rangeMinY = 100f;
+    [SerializeField]
+    private float rangeMaxY = 500f;
+    private GameObject[] asteroids;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        asteroids = new GameObject[]{ asteroid1B, asteroid1C, asteroid1D, asteroid1E,
+            asteroid4A, asteroid4B, asteroid5B, asteroid6A, asteroid6B, asteroid6C };
+        StartCoroutine(SpawnRoutine());      
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while(true)
+        {
+            float randomPosX = Random.Range(-rangeXZ, rangeXZ);
+            float randomPosZ = Random.Range(-rangeXZ, rangeXZ);
+            float randomPosY = Random.Range(rangeMinY, rangeMaxY);
+            Vector3 pos = new Vector3(randomPosX, randomPosY, randomPosZ);
+
+            int i = Random.Range(0, 10);
+            GameObject asteroid = Instantiate(asteroids[i]);
+            //asteroid.transform.SetParent(asteroidContainer.transform);
+            asteroid.transform.Translate(pos);
+            yield return new WaitForSeconds(1.0f);
+        }        
+    }
+}
